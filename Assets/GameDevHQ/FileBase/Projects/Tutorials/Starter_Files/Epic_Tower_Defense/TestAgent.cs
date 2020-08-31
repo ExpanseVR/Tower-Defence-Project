0491b2019ee4a467005605deb598123780c1e3e0
Michael Hayter <contact@michaelhayterphotography.com>
2020-08-31 13:51:56 +0800

    Navmesh baked in and navagent tested in TestAgent script. Target Object added

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/TestAgent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestAgent : MonoBehaviour
+{
+    [SerializeField] 
+    Transform _target;
+
+    NavMeshAgent _agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _agent.SetDestination(_target.position);
+        }
+    }
+}
