51c7d374afaa0f93f18e0890ec607224981b3454
Michael Hayter <contact@michaelhayterphotography.com>
2020-08-31 14:35:03 +0800

    AI system in place with AIController and Enemy scipt. Mech added in and walking animation set to default

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/AIController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    [SerializeField]
+    Transform _target;
+
+    private void OnEnable()
+    {
+        GetComponent<NavMeshAgent>().SetDestination(_target.position);
+    }
+}
---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/Epic_Tower_Defense/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private int _health;
+
+    [SerializeField]
+    private int _warfund;
+}
